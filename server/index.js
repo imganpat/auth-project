4c7ad402dd6d446dfc213b48edb94086c5c95d2a
Ganpat <coolganpat41@gmail.com>
2024-11-16 16:03:02 +0530

    fixed router problem

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-    origin: ["https://auth-project-gc.vercel.app", "http://localhost:3000"],
+    origin: true,
     credentials: true
 }))
 
